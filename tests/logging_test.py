9c5e0e815a9747e629cabee3091e4ee091c9bc7e
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-05-21 17:10:24 +0900

    Fix in logger

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -4,5 +4,5 @@ print (os.getcwd())
 from src.logger import get_logger
 
 if __name__ == "__main__":
-    log = get_logger()
+    log = get_logger(__name__)
     log.info("Testing logger, if log file created, then test passed successfully.")
