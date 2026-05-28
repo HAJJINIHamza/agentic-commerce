652e6fc8e314c4dea7cc60400a1181ce7869fbea
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-05-28 17:06:14 +0900

    Add pytest

--- tests/logging_test.py
+++
@@ -1,8 +0,0 @@
-import os 
-print (os.getcwd())
-
-from src.logger import get_logger
-
-if __name__ == "__main__":
-    log = get_logger(__name__)
-    log.info("Testing logger, if log file created, then test passed successfully.")
