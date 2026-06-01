1a360074822e10b904493e4ddf065471cffacf1a
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-06-01 17:33:55 +0900

    Add profit and margin simulation and tested them

--- agents/pricing_optimization/product_pricing.py
+++ agents/pricing_optimization/product_pricing.py
@@ -22,15 +22,19 @@ class ProductSellingPriceAgent:
 
         self.product_selling_dict["product_id"] = product_id
         
-        fixed_cost = self.compute_fixed_cost(product_id)
         total_rate = self.compute_total_rate(product_id)
+        if total_rate is None:
+            logger.info("Total rate is 1 or more, cannot compute the selling price.")
+            return None
+        
+        fixed_cost = self.compute_fixed_cost(product_id)
 
         product_price = fixed_cost / (1 - total_rate)
 
-        self.product_selling_dict["selling_price"] = product_price
-        logger.info(f"Selling price for product_id {product_id} is: {product_price}")
+        self.product_selling_dict["selling_price"] = round(product_price, 2)
+        logger.info(f"Selling price for product_id {product_id} is: {round(product_price, 2)}")
         logger.info(f"Product selling dict : {self.product_selling_dict}")
-        return product_price
+        return round(product_price, 2  )
 
     def compute_fixed_cost(self, product_id):
         """
@@ -64,7 +68,6 @@ class ProductSellingPriceAgent:
             "payment_fees": payment_fees
         }
         
-        print (f"Fixed costs is: {fixed_cost}")
         return fixed_cost
         
     def compute_total_rate(self, product_id):
@@ -94,5 +97,63 @@ class ProductSellingPriceAgent:
             "ad_cost_rate": ad_cost_rate,
             "target_margin_rate": target_margin_rate
         }
-        print (f"Total rate is: {total_rate}")
+        if total_rate >= 1:
+            logger.info("Total rate is 1 or more, impossible to make profit from this product.")
+            return None
         return total_rate
+    
+    #Simulate profit and margin given selling price 
+    def simulate_profit(self, product_id, simulated_selling_price ):
+        """
+        Simulate profit and margin for a given selling price
+        """
+    
+        if simulated_selling_price <= 0:
+            raise ValueError ("Invalid value for simulated_selling_price, param should be > 0")
+
+        total_rate = self.product_selling_dict.get("total_rate", self.compute_total_rate(product_id))
+        fixed_cost = self.product_selling_dict.get("fixed_cost", self.compute_fixed_cost(product_id))
+
+        net_revenue = simulated_selling_price * (1 - total_rate)
+        profit = net_revenue - fixed_cost
+        margin = profit / simulated_selling_price 
+
+        self.product_selling_dict["simulation_{simulated_selling_price}"] = {
+            "simulated_selling_price": simulated_selling_price,
+            "profit": profit,
+            "margin": margin
+        }
+
+        logger.info(f"Simulation for selling price {simulated_selling_price} : \
+                        profit is {round(profit, 2)}, margin is {round(margin, 2)  }")
+        
+        return round(profit, 2), round(margin, 2)
+    
+    #Simulate selling price given margin
+    def simulate_selling_price_given_margin (self, product_id, margin):
+        """
+        Simulate selling price for a given margin
+        Answering the question : At what price should we sell to make this margin ?
+        """
+
+        fixed_cost = self.product_selling_dict.get("fixed_cost", self.compute_fixed_cost(product_id))
+        total_rate = self.product_selling_dict.get("total_rate", self.compute_total_rate(product_id))
+
+        if total_rate + margin >= 1:
+            logger.info("Total rate + margin is 1 or more, impossible to make this margin from product")
+            return None
+            
+        simulated_selling_price = fixed_cost / (1 - total_rate - margin)
+
+        self.product_selling_dict["simulation_margin_{margin}"] = {
+            "margin": margin,
+            "simulated_selling_price": round(simulated_selling_price, 2)
+        }
+
+        return round(simulated_selling_price, 2) 
+
+
+
+        
+
+
--- tests/agents_testing/test_product_pricing.py
+++ tests/agents_testing/test_product_pricing.py
@@ -1,15 +1,27 @@
 import pandas as pd 
 from agents.pricing_optimization.product_pricing import ProductSellingPriceAgent
 
-def test_get_product_selling_price():
-    data_test = pd.read_csv("data/test_product_iter_1.csv", sep=";")
-    product_pricing_agent = ProductSellingPriceAgent(data_test)
-    product_id = data_test["id"].iloc[0]
-    selling_price = product_pricing_agent.get_product_selling_price(product_id)
-    return selling_price
+data_test = pd.read_csv("data/test_product_iter_1.csv", sep=";")
+product_pricing_agent = ProductSellingPriceAgent(data_test)
+product_id = data_test["id"].iloc[0]
+
 
 if __name__ == "__main__":
     #FOR TESTING RUN FOLLOWING COMMAND
     # python -m tests.agents_testing.product_pricing_test
-    selling_price = test_get_product_selling_price()
-    print ("Selling price : ", selling_price)
+    selling_price = product_pricing_agent.get_product_selling_price(product_id)
+    print ("Selling price should be : ", selling_price)
+
+    simulated_selling_price = input("Enter a selling price to simulate profit and margin >> ")
+    simulated_selling_price = float(simulated_selling_price)
+    profit, margin = product_pricing_agent.simulate_profit(product_id, 
+                                                           simulated_selling_price=simulated_selling_price)
+    print ("Profit will be : ", profit)
+    print ("Margin will be : ", margin)
+
+    simulate_margin = input("Enter a margin to simulate the required selling price >> ")
+    simulate_margin = float(simulate_margin)
+    selling_price_for_margin = product_pricing_agent.simulate_selling_price_given_margin(product_id, 
+                                                                              margin=simulate_margin)
+    print (f"Selling price to achieve margin {simulate_margin}  is : {selling_price_for_margin}")
+
