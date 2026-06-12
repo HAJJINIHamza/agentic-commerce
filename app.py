0b3a3a8d786454862337f601085d2946940cebb5
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-06-12 17:36:52 +0900

    Add lesting evaluator page

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("LLM Core AI Agentic Commerce")
 
 st.write("Please select page to start")
+
+#TO RUN USE COMMAND : 
+#streamlit run app.py
--- pages/listing_page.py
+++ pages/listing_page.py
@@ -8,6 +8,7 @@ if str(PROJECT_ROOT) not in sys.path:
 
 import streamlit as st 
 from agents.listing_generation.listing_generator import listingGenerationAgent
+from agents.listing_generation.listing_evaluator import listingEvaluatorAgent
 
 st.title("Product Listings Agent")
 
@@ -29,32 +30,58 @@ if st.session_state.manual_mode:
         submit_form = st.form_submit_button("Generate")
 
         if submit_form:
-            with st.empty():
-                st.info("⏳ Generating listings, please wait...")
-                listings, tiktok_listings = listingGenerationAgent().generate_listings(target_country,
-                                                                                   product_name,
-                                                                                   product_category,
-                                                                                   main_benefit,
-                                                                                   buyer_persona,
-                                                                                   forbidden_claims
-                                                                                   )
-                st.empty()
+            if not all([
+                product_name.strip(),
+                product_category.strip(),
+                main_benefit.strip()
+            ]):
+                st.error("The following fields are required : Product name, Product catefory and main benifit.")
 
-            st.subheader("Product listings")
-            
-            st.write(f"**Title** : {listings["title"]}")
-            st.write(f"**Key words** :" )
-            for key_word in listings["keywords"]:
-                st.write(f"- {key_word}")
-            st.write(f"**Description** :")
-            for description in listings["description"]:
-                st.write(f"- {description}")
-            st.write(f"**Safe claims** :")
-            for safe_claim in listings["safe_claims"]:
-                st.write(f"- {safe_claim}")
-            st.write(f"**TikTok hook** : {tiktok_listings["tiktok_hook"]}")
-            st.write(f"**TikTok video script** : {tiktok_listings["tiktok_short_video_script"]}")
+            else :
+                #Generate listings 
+                with st.empty():
+                    st.info("⏳ Generating listings, please wait...")
+                    listings, tiktok_listings = listingGenerationAgent().generate_listings(target_country,
+                                                                                    product_name,
+                                                                                    product_category,
+                                                                                    main_benefit,
+                                                                                    buyer_persona,
+                                                                                    forbidden_claims
+                                                                                    )
+                    st.empty()
 
-            #st.write (f"listings :")
-            #st.write (listings)
-            #st.write (tiktok_listings)
+                st.subheader("Product listings")
+                
+                st.write(f"**Title** : {listings["title"]}")
+                st.write(f"**Key words** :" )
+                for key_word in listings["keywords"]:
+                    st.write(f"- {key_word}")
+                st.write(f"**Description** :")
+                for description in listings["description"]:
+                    st.write(f"- {description}")
+                st.write(f"**Safe claims** :")
+                for safe_claim in listings["safe_claims"]:
+                    st.write(f"- {safe_claim}")
+                st.write(f"**TikTok hook** : {tiktok_listings["tiktok_hook"]}")
+                st.write(f"**TikTok video script** : {tiktok_listings["tiktok_short_video_script"]}")
+
+                with st.empty():
+                    st.info("⏳ Evaluating listings, please wait...")
+                    eval_results = listingEvaluatorAgent().evaluate_listings(product_name,
+                                                                            product_category,
+                                                                            forbidden_claims,
+                                                                            listings["title"],
+                                                                            listings["keywords"],
+                                                                            listings["description"],
+                                                                            listings["safe_claims"],
+                                                                            tiktok_listings["tiktok_hook"],
+                                                                            tiktok_listings["tiktok_short_video_script"])
+                    st.empty()
+
+                #Evaluate listings
+                if eval_results["accepted"] == True:
+                    st.success("Listings are safe to be pusblished")
+                
+                else :
+                    st.error("Listings have been rejected by AI juge")
+                    st.info(f"Reason : {eval_results["reason"]}")
