a2170e0e4be4b5e9b7216cfa61ea67dd01472bb6
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-08-03 16:23:37 +0900

    Edit supplier scoring accoriding to experiments

--- agents/supplier_scoring/supplier_scoring.py
+++ agents/supplier_scoring/supplier_scoring.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np
-from math import log
+from math import log, exp
 
 from src.logger import get_logger
 
@@ -11,7 +11,10 @@ class supplierScoringAgent:
         self.supplier_data = supplier_data
         # Ensure that numeric columns are in the correct format
         num_columns = [col for col in self.supplier_data.columns if col not in \
-                       ["id", "product_name", "product_category", "supplier_name"]]
+                       ["id", "product_name", "product_category", "supplier_name", 
+                        "supplier_id", "product_id", "supplier_name", "product_name", 
+                        "supplier_product_name", "place_of_origin", "supplier_url"
+                        ]]
         self.supplier_data[num_columns] = (self.supplier_data[num_columns]
                                         .replace(",", ".", regex=True)
                                         .apply(pd.to_numeric, errors="coerce"))
@@ -23,24 +26,32 @@ class supplierScoringAgent:
         Computes the supplier reliability score for a product
         """
         logger.info ("Computing supplier score")
-        supplier_rating_score = self.compute_supplier_rating_score(supplier_id)
+        #supplier_rating_score = self.compute_supplier_rating_score(supplier_id)
+        supplier_product_cost_score = self.compute_supplier_product_cost_score(supplier_id)
         moq_score = self.compute_moq_score(supplier_id)
-        supplier_response_rate_score = self.compute_supplier_response_rate_score(supplier_id)
-        order_volume_score = self.compute_order_volume_score(supplier_id)
+        supplier_tenure_score = self.compute_supplier_tenure_score(supplier_id)
+        place_of_origin_score = self.compute_place_of_origin_score(supplier_id)
+        supplier_response_time_score = self.compute_supplier_response_time_score(supplier_id)
+        images_quality_score = self.compute_supplier_images_quality(supplier_id)
+        #order_volume_score = self.compute_order_volume_score(supplier_id)
 
-        supplier_score = (0.3 * supplier_rating_score + 
-                                    0.3 * moq_score + 
-                                    0.2 * supplier_response_rate_score + 
-                                    0.2 * order_volume_score)
+        supplier_score = (0.4 * supplier_product_cost_score
+                          + 0.3 * moq_score
+                          + 0.1 * images_quality_score
+                          + 0.05 * supplier_tenure_score
+                          + 0.05 * place_of_origin_score
+                          + 0.1 * supplier_response_time_score)
         
         classification = self.classify_supplier(supplier_id, supplier_score)
 
         self.supplier_score_dict =  {
             "supplier_score" : supplier_score,
-            "supplier_rating_score": supplier_rating_score,
+            "supplier_product_cost_score": supplier_product_cost_score,
             "moq_score": moq_score,
-            "supplier_response_rate_score": supplier_response_rate_score,
-            "order_volume_score": order_volume_score,
+            "supplier_tenure_score": supplier_tenure_score,
+            "place_of_origin_score": place_of_origin_score,
+            "supplier_response_time_score": supplier_response_time_score,
+            "images_quality_score": images_quality_score,
             "classification": classification
         }
 
@@ -48,18 +59,58 @@ class supplierScoringAgent:
         logger.info (f"Supplier score details : {self.supplier_score_dict}")
         return supplier_score, self.supplier_score_dict
     
-    def compute_supplier_rating_score(self, supplier_id):
+    def compute_supplier_product_cost_score(self, supplier_id):
         """
-        Computes the supplier rating score for a product
+        Computes the score for the cost of a product from supplier_id
         """
-        print ("Computing supplier rating score")
-        supplier_rating = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id, 
-                                            "supplier_rating"].values[0]
+        print ("Computing supplier product cost score")
+        product_cost = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id, 
+                                            "product_cost"].values[0]
+        
+        product_cost_score = 1/(1+ product_cost)
+        product_cost_score = min(product_cost_score, 1)  # Cap the score at 1
+        logger.info (f"Product cost score for supplier {supplier_id} is : {product_cost_score}")
 
-        supplier_rating_score = supplier_rating / 5
-        print ("supplier_rating_score is : ", supplier_rating_score)
-        return supplier_rating_score
+        return product_cost_score
+    
+    def compute_place_of_origin_score(self, supplier_id):
+        """
+        Computes the score for the place of origin of a product from supplier_id
+        """
+        print ("Computing place of origin score")
+        place_of_origin = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id, 
+                                            "place_of_origin"].values[0]
+        
+        if place_of_origin is None:
+            raise ValueError(f"Place of origin for supplier {supplier_id} is None  ")
+        
+        elif place_of_origin == "korea":
+            place_of_origin_score = 1.0
+        
+        else : 
+            place_of_origin_score = 0.1
+        
+        logger.info (f"Place of origin score for supplier {supplier_id} is : {place_of_origin_score}")
 
+        return place_of_origin_score
+
+    def compute_supplier_tenure_score(self, supplier_id):
+        """
+        Computes the score for the tenure of a supplier
+        """
+
+        supplier_tenure = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id,
+                                            "supplier_tenure_y"].values[0]
+        max_supplier_tenure = self.supplier_data["supplier_tenure_y"].max()
+
+        if max_supplier_tenure == 0:
+            raise ValueError(f"Max supplier tenure is zero, cannot compute tenurescore for supplier {supplier_id}")
+        
+        supplier_tenure_score = log(supplier_tenure + 1)/log(max_supplier_tenure + 1)
+
+        logger.info (f"Supplier tenure score for supplier {supplier_id} is : {supplier_tenure_score}")
+        return supplier_tenure_score
+    
     def compute_moq_score(self, supplier_id):
         """
         Computes the minimum order quantity score for a product
@@ -75,19 +126,50 @@ class supplierScoringAgent:
         moq_score = 1 / log(moq + 1)
         print (f"moq_score is : {moq_score}")
         return moq_score
-
-    def compute_supplier_response_rate_score(self, supplier_id):
+    
+    def compute_supplier_response_time_score(self, supplier_id):
         """
-        Computes the supplier response rate score for a product
+        Computes the supplier response time score for a product
         """
-        print ("Computing supplier response rate score")
-        response_rate = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id, 
-                                            "supplier_response_rate"].values[0]
-        supplier_response_rate_score = response_rate / 100
+        print ("Computing supplier response time score")
+        response_time = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id, 
+                                            "supplier_response_time_h"].values[0]
+        supplier_response_time_score = 1 / (1 + response_time)
 
-        print (f"supplier response rate score is : {supplier_response_rate_score}")
+        print (f"supplier response time score is : {supplier_response_time_score}")
 
-        return supplier_response_rate_score
+        return supplier_response_time_score
+    
+    def compute_supplier_images_quality(self, supplier_id):
+        """"
+        Computes the score for the images quality of a product from supplier_id
+        """
+        images_quality = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id,
+                                            "images_quality"].values[0]
+        
+        images_quality_score = images_quality / 5  # Quality is rated from 1 to 5.
+
+        logger.info (f"Images quality score for supplier {supplier_id} is : {images_quality_score}")
+        return images_quality_score
+    
+    
+    ### -------------------------------------------------------------------------
+    ### Can't use the functions bellow for now because we don't have the data yet.
+    ### -------------------------------------------------------------------------
+    
+    def compute_supplier_rating_score(self, supplier_id):
+        """
+        Computes the supplier rating score for a product
+        """
+        print ("Computing supplier rating score")
+        supplier_rating = self.supplier_data.loc[self.supplier_data["supplier_id"] == supplier_id, 
+                                            "supplier_rating"].values[0]
+
+        supplier_rating_score = supplier_rating / 5
+        print ("supplier_rating_score is : ", supplier_rating_score)
+        logger.info ("supplier_rating_score is : ", supplier_rating_score)
+
+        return supplier_rating_score
 
     def compute_order_volume_score(self, supplier_id):
         """
--- pages/supplier_board.py
+++ pages/supplier_board.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px 
 from agents.supplier_scoring.supplier_scoring import supplierScoringAgent
+from agents.supplier_scoring.supplier_contacting import ContactSupplier
 
 st.set_page_config(page_title="LLM Core AI (Inc) Agentic Commerce") 
 
@@ -29,13 +30,16 @@ if uploaded_file:
             "supplier_name": [],
             "product_name": [],
             "supplier_score" : [],
-            "supplier_rating_score": [],
+            "product_cost_score": [],
             "moq_score": [],
-            "supplier_response_rate_score": [],
-            "order_volume_score": [],
+            "images_quality_score": [],
+            "supplier_response_time_score": [],
+            "supplier_tenure_score": [],
+            "place_of_origin_score": [],
             "classification": []
         }
-
+    
+    print ("supplier name :", suppliers_data["supplier_name"])
     for supplier_id in suppliers_data["supplier_id"].values:
         supplier_scoring_agent = supplierScoringAgent(suppliers_data)
         supplier_score, score_components = supplier_scoring_agent.compute_supplier_score(supplier_id)
@@ -43,16 +47,17 @@ if uploaded_file:
         df["supplier_id"].append(supplier_id)
         df["product_id"].append(suppliers_data.loc[suppliers_data["supplier_id"] == supplier_id, 
                                                    "product_id"].iloc[0])
-        print ("supplier names :", suppliers_data["supplier_name"])
         df["supplier_name"].append(suppliers_data.loc[suppliers_data["supplier_id"] == supplier_id, 
                                                    "supplier_name"].iloc[0])
         df["product_name"].append(suppliers_data.loc[suppliers_data["supplier_id"] == supplier_id, 
                                                    "product_name"].iloc[0])
         df["supplier_score"].append(supplier_score)
-        df["supplier_rating_score"].append(score_components["supplier_rating_score"])
+        df["product_cost_score"].append(score_components["supplier_product_cost_score"])
         df["moq_score"].append(score_components["moq_score"])
-        df["supplier_response_rate_score"].append(score_components["supplier_response_rate_score"])
-        df["order_volume_score"].append(score_components["order_volume_score"])
+        df["images_quality_score"].append(score_components["images_quality_score"])
+        df["supplier_response_time_score"].append(score_components["supplier_response_time_score"])
+        df["supplier_tenure_score"].append(score_components["supplier_tenure_score"])
+        df["place_of_origin_score"].append(score_components["place_of_origin_score"])
         df["classification"].append(score_components["classification"])
 
     df = pd.DataFrame(df)
@@ -63,11 +68,12 @@ if uploaded_file:
  
     top_supplier = df.iloc[0] 
     score_component_names = [
-                            "supplier_rating_score", 
+                            "product_cost_score", 
                             "moq_score", 
-                            "supplier_response_rate_score", 
-                            "order_volume_score",
-                            "classification" 
+                            "images_quality_score",
+                            "supplier_response_time_score", 
+                            "supplier_tenure_score",
+                            "place_of_origin_score",    
                             ]
     
     #fig, ax = plt.subplots(figsize=(10, 6))
@@ -102,6 +108,10 @@ if uploaded_file:
     ##st.pyplot(fig)
     st.plotly_chart(fig, use_container_width=True)
 
+    st.write("TOP supplier information")
+    top_supplier_data = suppliers_data[suppliers_data["supplier_id"] == top_supplier["supplier_id"]]
+    st.dataframe(top_supplier_data)
+
 
     if top_supplier["classification"] == "Excelent":
         st.success("Excelent supplier, strong candidate for testing")
@@ -114,6 +124,16 @@ if uploaded_file:
     
     else : 
         st.error("Bad supplier, to avoid")
+    
+    st.subheader("Email to send to supplier")
+    top_supplier_id = top_supplier["supplier_id"]
+    product_name = suppliers_data.loc[suppliers_data["supplier_id"] == supplier_id, "product_name"].values[0]
+    product_cost = suppliers_data.loc[suppliers_data["supplier_id"] == supplier_id, "product_cost"].values[0]
+    moq = suppliers_data.loc[suppliers_data["supplier_id"] == supplier_id, "moq"].values[0]
+    email = ContactSupplier().get_contacting_mail(product_name, 
+                                                  product_cost,
+                                                  moq)
+    st.write(email)
     
 
 #USE THIS COMMAND TO LAUNCH THE APP : streamlit run pages/product_radar.py
