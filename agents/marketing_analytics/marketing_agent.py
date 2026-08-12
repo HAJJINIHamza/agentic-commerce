ceaa0e09e4aa27976fe1b20a1f5a86a4fa89bfe7
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-08-12 15:59:14 +0900

    Developping marketing_agent

--- agents/marketing_analytics/marketing_agent.py
+++ agents/marketing_analytics/marketing_agent.py
@@ -4,7 +4,7 @@ from src.logger import get_logger
 
 logger = get_logger(__name__)
 
-class MarketingAgent:
+class marketingAgent:
     def __init__(self):
         pass
 
@@ -35,13 +35,36 @@ class MarketingAgent:
         Compute marketing metrics from Shopee Ads data
         """
         shopee_ads_data = shopee_ads_data[shopee_ads_data["Status"] == "Ongoing"]
-        cpc = shopee_ads_data["Expense"]/shopee_ads_data["Clicks"]
-        cr = shopee_ads_data["Orders"]/shopee_ads_data["Clicks"]
-        if shopee_ads_data["Clicks"] != 0:
-            number_of_impressions_needed = shopee_ads_data["Clicks"]/shopee_ads_data["CTR"]
-        if shopee_ads_data["orders"] != 0:
-            number_of_clicks_needed = shopee_ads_data["orders"]/shopee_ads_data["cr"]
-        
+        expense = float(shopee_ads_data["Expense"].values[0])
+        clicks = int(shopee_ads_data["Clicks"].values[0])
+        cpc = expense/clicks if clicks != 0 else None
+        conversions = int(shopee_ads_data["Conversions"].values[0])
+        conversion_rate = float(shopee_ads_data["Conversion Rate"].values[0].replace("%", ""))
+        ctr = float(shopee_ads_data["CTR"].values[0].replace("%", ""))
 
-        return cpc, cr, 
+        number_of_impressions_needed = 100/ctr if ctr != 0 else None #100 for percentage (1/ctr  * 100)
+
+        number_of_clicks_needed = 100/conversion_rate if conversion_rate != 0 else None
+
+        marketing_metrics = {
+                "date_period" : shopee_ads_data["Date Period"].values[0],
+                "ad_name": shopee_ads_data["Ad Name"].values[0],
+                "ctr_%": ctr,
+                "cpc_$": cpc,
+                "conversion_rate_%": conversion_rate,
+                "number_of_impressions_needed": number_of_impressions_needed,
+                "number_of_clicks_needed": number_of_clicks_needed
+                }
+        
+        logger.info(f"Marketing metrics : {marketing_metrics}")
+
+        return marketing_metrics
+
+if __name__ == "__main__":
+    file_path = "data/marketing/shopee_ads/Shopee-Ads-Overall-Data-05_08_2026-11_08_2026.csv"
+    marketing_agent = marketingAgent()
+    shopee_ads_data = marketing_agent.read_shopee_ads_data(file_path)
+    marketing_metrics = marketing_agent.compute_marketing_metrics(shopee_ads_data)
+    print (marketing_metrics)
+
 
